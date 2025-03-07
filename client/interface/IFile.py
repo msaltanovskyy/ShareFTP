c880163aae0940ebcb18d49012b538769d289a62
msaltanovskyy <bust.cot@gmail.com>
2025-03-07 15:54:21 +0100

    Refactor IFile interface methods for improved clarity and functionality

--- client/interface/IFile.py
+++ client/interface/IFile.py
@@ -1,31 +1,25 @@
 from abc import ABC, abstractmethod
+from typing import List, Any
 
 
 class IFile(ABC):
-    
+
     @abstractmethod
-    def select_dir():
+    def select_and_verify_dir(self) -> str:
         pass
-    
+
     @abstractmethod
-    def proof_correct_dir(dir):
+    def get_files_list(self, files: List[str], dir_path: str) -> None:
         pass
-    
+
     @abstractmethod
-    def file_send_quantity(address):
+    def select_files(self, dir_path: str) -> List[str]:
         pass
-    
+
     @abstractmethod
-    def select_file():
+    def send_files(self, address, files: List[str]) -> None:
         pass
-    
+
     @abstractmethod
-    def get_files_list(files, file_path):
+    def file_send(self, chose_func) -> None:
         pass
-    
-    @abstractmethod
-    def file_send(chose_func):
-        pass
-    
-    
-    
